943b30c8294fe3cc2558576ba782c8dd24a68079
A-plugin <aplugin23@gmail.com>
2023-12-09 22:10:28 +0900

    base 0.6

--- src/main/java/org/apo/pluginbase/Commands/Commands.java
+++ src/main/java/org/apo/pluginbase/Commands/Commands.java
@@ -3,6 +3,7 @@ package org.apo.pluginbase.Commands;
 import org.apo.pluginbase.PluginBase;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Color;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -28,28 +29,28 @@ public class Commands implements CommandExecutor {
                     config= pb.getConfig();
                     Bukkit.setMotd(Objects.requireNonNull(config.getString("motd.txt")
                             .replace("&", "§")));
-                    p.sendMessage(ChatColor.GREEN+"Base plugin reload success.");
+                    p.sendMessage(Color.GREEN+"Base plugin reload success.");
                 } else if (args[0].equalsIgnoreCase("list")) {
                     pb.getData();
                     p.sendMessage(ChatColor.GREEN+"====BASE====\n" +
-                            ChatColor.BLUE+"title : "+ChatColor.WHITE+pb.title+
-                            ChatColor.BLUE+"\nsub title : "+ChatColor.WHITE+pb.sub+
-                            ChatColor.BLUE+"\njoinmessage : "+ChatColor.WHITE+pb.jm+
-                            ChatColor.BLUE+"\nleftmessage : "+ChatColor.WHITE+ pb.lm+
-                            ChatColor.BLUE+"\nChatting : "+ChatColor.WHITE+ pb.chat +
-                            ChatColor.BLUE+"\nmotd : "+ChatColor.WHITE+config.getString("motd.txt")+
-                            ChatColor.BLUE+"\nTab header : " +ChatColor.WHITE+ pb.header+
-                            ChatColor.BLUE+"\nTab footer : " +ChatColor.WHITE+ pb.footer+
-                            ChatColor.GREEN+"\n========="
+                            Color.BLUE+"title : "+Color.WHITE+pb.title+
+                            Color.BLUE+"\nsub title : "+Color.WHITE+pb.sub+
+                            Color.BLUE+"\njoinmessage : "+Color.WHITE+pb.jm+
+                            Color.BLUE+"\nleftmessage : "+Color.WHITE+ pb.lm+
+                            Color.BLUE+"\nChatting : "+Color.WHITE+ pb.chat +
+                            Color.BLUE+"\nmotd : "+Color.WHITE+config.getString("motd.txt")+
+                            Color.BLUE+"\nTab header : " +Color.WHITE+ pb.header+
+                            Color.BLUE+"\nTab footer : " +Color.WHITE+ pb.footer+
+                            Color.GREEN+"\n========="
                     );
                 } else if (args[0].equalsIgnoreCase("motd")) {
                     if (args.length>=1) {
                         pb.getServer().setMotd(config.getString("motd.txt").replace("&", "§"));
-                        p.sendMessage(ChatColor.RED+"Motd Set");
+                        p.sendMessage(Color.RED+"Motd Set");
                     } else {
                         pb.getConfig().set("motd.txt", args[1]);
                         pb.getServer().setMotd(args[1].replace("&", "§"));
-                        p.sendMessage(ChatColor.GREEN+"Motd set"+args[1]);
+                        p.sendMessage(Color.GREEN+"Motd set"+args[1]);
                         pb.saveConfig();
                     }
                 } else if (args[0].equalsIgnoreCase("")) {
@@ -57,9 +58,9 @@ public class Commands implements CommandExecutor {
                 }
             } else {
                 if (p.getLocale().startsWith("ko")) {
-                    p.sendMessage(ChatColor.RED+"당신은 권한을 가지고 있지 않습니다!");
+                    p.sendMessage(Color.RED+"당신은 권한을 가지고 있지 않습니다!");
                 } else {
-                    p.sendMessage(ChatColor.RED+"You don't have permission");
+                    p.sendMessage(Color.RED+"You don't have permission");
                 }
             }
         } else {
